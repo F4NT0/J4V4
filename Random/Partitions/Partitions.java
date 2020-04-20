7c9dcd7d9bfffb955bd0db31f30b0d6361a0109f
F4NT0 <gabriel.stundner@acad.pucrs.br>
2020-04-20 20:20:47 -0300

    try partition

---
+++ Random/Partitions/Partitions.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class Partitions {
+    private int memorySize = 1024;
+    private ArrayList<Object> memory;
+    private Integer size = 4;
+
+    public int lastPosition(int size){
+        
+    }
+
+    public Partitions(){
+        int division = memorySize / size;
+
+    }
+
+    public static void main(String[] args){
+        
+    }
+}
