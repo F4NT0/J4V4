b3feb4b4aa7dcad877c72cd2a223636d8893daae
F4NT0 <gabriel.stundner@acad.pucrs.br>
2020-04-22 20:18:42 -0300

    somatorio recursivo

--- Random/somatorio_recursivo/Somatory.java
+++ Random/somatorio_recursivo/Somatory.java
@@ -1,16 +1,17 @@
 public class Somatory{
     // Função Recursiva
-    public void somatorio(int[] values,int initialPosition,int prevValue){
-        if(initialPosition < values.length){
-            int value = prevValue + values[initialPosition];
-            System.out.println( prevValue + " + " + values[initialPosition] + " = " + value);
-            somatorio(values,initialPosition+1, value);
+    public int somatorio(int[] values,int initialPosition){
+        if(initialPosition == values.length){
+            return 0; 
         }
+        int result = values[initialPosition] + somatorio(values,initialPosition+1);
+        return result; 
     }
 
     public static void main(String[] args){
         Somatory somatorio = new Somatory();
         int[] values = {1,20,7,2};
-        somatorio.somatorio(values, 0, 0); // 3 posições
+        int soma = somatorio.somatorio(values, 0); // 3 posições
+        System.out.println("Soma: " + soma);
     }
 }
