92069de8d4923d08c5a10103fcca8834918b0e4e
Gabriel Fanto Stundner <gabriel.stundner@acad.pucrs.br>
2022-09-06 13:06:22 -0300

    Binary to Decimal

---
+++ Basic/Binary.java
@@ -0,0 +1,30 @@
+public class Binary {
+    public static void main(String[] args) {
+        
+        
+        int[] binary = {0,1,0,0};
+        int decimalValue = 0;
+        int index = 0;
+
+        while (index < binary.length) {
+            decimalValue += binary[index] * Math.pow(2,index);
+            index++;
+        }
+
+        System.out.print("Binary: ");
+        
+        for (int bit : binary) {
+            System.out.print(bit); // 0100
+        }
+
+        System.out.println("\nDecimal Number: " + decimalValue); //2
+    }
+
+    // Table test
+    // index | binary.length |binary[index] | decimalValue
+    //   0   |       4       |      0       |   0 + (0 * 2^0) = 0
+    //   1   |       4       |      1       |   0 + (1 * 2^1) = 0 + 2 = 2
+    //   2   |       4       |      0       |   2 + (0 * 2^2) = 2 + 0 = 2
+    //   3   |       4       |      0       |   2 + (0 * 2^3) = 2 + 0 = 2
+    // Result: 2
+}
